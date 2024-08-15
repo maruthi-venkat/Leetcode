1ff8aacf707066732f544505504a3ad9b723ec46
Venkat Reddy Nareddula <130493194+maruthi-venkat@users.noreply.github.com>
2024-08-15 19:44:24 +0530

    Time: 6 ms (35.52%) | Memory: 12.3 MB (6.14%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(i >= triangle.size()) return 0;
+    // Bottom Up approach
 
-        if(dp[i][j] != -1) return dp[i][j];
+    // int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+    //     if(i >= triangle.size()) return 0;
 
-        int down = triangle[i][j] + solve(i+1,j,triangle,dp);
-        int dnext = triangle[i][j] + solve(i+1,j+1,triangle,dp);
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     int down = triangle[i][j] + solve(i+1,j,triangle,dp);
+    //     int dnext = triangle[i][j] + solve(i+1,j+1,triangle,dp);
         
-        int res = min(down,dnext);
+    //     int res = min(down,dnext);
 
-        return dp[i][j] = res;
-    }
+    //     return dp[i][j] = res;
+    // }
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp;
@@ -24,7 +27,30 @@ public:
             dp.push_back(temp);
             j++;
         }
-        
-        return solve(0,0,triangle,dp);
+
+        // return solve(0,0,triangle,dp);
+
+        // space optimization
+        dp[0][0] = triangle[0][0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                    dp[i][j] = triangle[i][j] + dp[i-1][j];
+                }
+                else if(j!=i){
+                    dp[i][j] = triangle[i][j] + min(dp[i-1][j],dp[i-1][j-1]);
+                }
+                else{
+                    dp[i][j] = triangle[i][j] + dp[i-1][j-1];
+                }
+            }
+        }
+
+        int mini = INT_MAX;
+        for(int i=0;i<dp[n-1].size();i++){
+            mini = min(mini,dp[n-1][i]);
+        }
+
+        return mini;
     }
 };
