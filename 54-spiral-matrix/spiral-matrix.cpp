ce61782394867586cc8d3ab06fb9da60a8983b3a
Venkat Reddy Nareddula <130493194+maruthi-venkat@users.noreply.github.com>
2024-10-05 11:48:53 +0530

    Time: 3 ms (36.68%) | Memory: 8.5 MB (58.37%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
         int l = 0;
-        int r = m-1;
+        int r = cols - 1;
         int u = 0;
-        int d = n-1;
+        int d = rows - 1;
 
         vector<int> res;
         while(l<=r && u<=d){
-            for(int i=l;i<=r;i++){
+            for(int i = l;i<=r;i++){
                 res.push_back(matrix[u][i]);
             }
             u++;
-            if(u>d){
+            if(u > d){
                 break;
             }
-            for(int i=u;i<=d;i++){
+
+            for(int i = u;i<=d;i++){
                 res.push_back(matrix[i][r]);
             }
             r--;
-            if(r<l){
+            if(l > r){
                 break;
             }
+
             for(int i=r;i>=l;i--){
                 res.push_back(matrix[d][i]);
             }
             d--;
-            if(d<u){
+            if(u > d){
                 break;
             }
+
             for(int i=d;i>=u;i--){
                 res.push_back(matrix[i][l]);
             }
             l++;
-            if(l>r){
+            if(l > r){
                 break;
             }
         }
