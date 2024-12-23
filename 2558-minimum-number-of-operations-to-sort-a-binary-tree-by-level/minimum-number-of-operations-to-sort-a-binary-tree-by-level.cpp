0ebe0a8f57b139ac22f5e436cef3b3bbec2905c3
Venkat Reddy Nareddula <130493194+maruthi-venkat@users.noreply.github.com>
2024-12-23 23:31:04 +0530

    Time: 692 ms (12.31%) | Memory: 182.5 MB (69.21%) - LeetSync

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+
+    void helper(vector<int> arr,vector<int> actual){
+        sort(actual.begin(),actual.end());
+        // cout<<actual[0]<<actual[actual.size()-1]<<endl;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] != actual[i]){
+                for(int j=i+1;j<arr.size();j++){
+                    if(arr[j] == actual[i]){
+                        swap(arr[i],arr[j]);
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void levelorder(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> arr;
+            for(int i=0;i<n;i++){
+                auto curr = q.front();
+                arr.push_back(curr -> val);
+                if(curr -> left)
+                    q.push(curr -> left);
+                if(curr -> right)
+                    q.push(curr -> right);
+
+                q.pop();
+            }
+            helper(arr,arr);
+           
+        }
+    }
+
+    int minimumOperations(TreeNode* root) {
+        levelorder(root);
+        return count;
+    }
+};
