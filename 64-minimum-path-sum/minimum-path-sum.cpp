37d959c46290434868b0ebdbe1250e7c5e334d91
Venkat Reddy Nareddula <130493194+maruthi-venkat@users.noreply.github.com>
2025-01-21 19:28:25 +0530

    Time: 4 ms (17.12%) | Memory: 16.5 MB (13.71%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,85 +1,22 @@
 class Solution {
 public:
-    // Top down approach
+    int solve(int i,int j,vector<vector<int>> &grid,vector<vector<int>> &dp,int n,int m){
+        if(i == n-1 && j == m-1) return grid[i][j];
 
-    // long solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
-    //     if(i<0 || j<0) return INT_MAX;
+        if(i >= n || j >= m) return 600000;
 
-    //     if(i==0 && j==0) return grid[0][0];
-        
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     long top = grid[i][j] + solve(i-1,j,grid,dp);
-    //     long left = grid[i][j] + solve(i,j-1,grid,dp);
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     long res = min(top,left);
+        int right = grid[i][j] + solve(i,j+1,grid,dp,n,m);
+        int down = grid[i][j] + solve(i+1,j,grid,dp,n,m);
 
-    //     return dp[i][j] = res;
-    // }
+        return dp[i][j] = min(right,down);
+    }
 
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        // vector<vector<int>> dp(n,vector<int> (m,-1));
-
-        // return (int)solve(n-1,m-1,grid,dp);
-
-        // Bottom Up approach
-
-        // dp[0][0] = grid[0][0];
-
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(i == 0 && j == 0) continue;
-
-        //         if(i>0 && j>0){
-        //             dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
-        //         }
-        //         else{
-        //             if(j > 0){
-        //                 dp[i][j] = grid[i][j] + dp[i][j-1];
-        //             }
-        //             else if(i > 0){
-        //                 dp[i][j] = grid[i][j] + dp[i-1][j];
-        //             }
-        //             else{
-        //                 dp[i][j] = grid[i][j];
-        //             }
-        //         }
-        //     }
-        // }
-
-
-        // return dp[n-1][m-1];
-
-        vector<int> prev(m,-1);
-        vector<int> curr(m,-1);
-
-        curr[0] = grid[0][0];
-        for(int row=0;row<n;row++){
-            for(int i=0;i<m;i++){
-                if(i == 0){
-                    if(prev[i] == -1){
-                        curr[i] = grid[row][i];
-                    }
-                    else{
-                        curr[i] = grid[row][i] + prev[i];
-                    }
-                }
-                else{
-                    if(prev[i] == -1){
-                        curr[i] = grid[row][i] + curr[i-1];
-                    }
-                    else{
-                        curr[i] = grid[row][i] + min(curr[i-1],prev[i]);
-                    }
-                }
-            }
-
-            for(int i=0;i<m;i++){
-                prev[i] = curr[i];
-            }
-        }
-        
-        return curr[m-1];
+        vector<vector<int>> dp(n, vector<int> (m,-1));
+        return solve(0,0,grid,dp,n,m);
     }
 };
