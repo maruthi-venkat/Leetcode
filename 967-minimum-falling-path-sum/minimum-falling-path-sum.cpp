0e6755b625c58b8f0c7dfb2f7994619614eb4b0c
Venkat Reddy Nareddula <130493194+maruthi-venkat@users.noreply.github.com>
2025-01-23 19:16:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (16.65%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,64 +1,48 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>> &dp,int n){
+        if(i >= n) return 0;
+        if(j < 0) return 10000;
+        if(j >= n) return 10000;
 
-    // long solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-    //     if(i >= matrix.size()){
-    //         return 0;
-    //     }
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     long dleft = INT_MAX;
-    //     long down = INT_MAX;
-    //     long dright = INT_MAX;
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     if(j==0){
-    //         down = matrix[i][j] + solve(i+1,j,matrix,dp);
-    //         dright = matrix[i][j] + solve(i+1,j+1,matrix,dp);
-    //     }
-    //     else if(j == matrix.size()-1){
-    //         down = matrix[i][j] + solve(i+1,j,matrix,dp);
-    //         dleft = matrix[i][j] + solve(i+1,j-1,matrix,dp);
-    //     }
-    //     else{
-    //         down = matrix[i][j] + solve(i+1,j,matrix,dp);
-    //         dleft = matrix[i][j] + solve(i+1,j-1,matrix,dp);
-    //         dright = matrix[i][j] + solve(i+1,j+1,matrix,dp);
-    //     }
+        int left = INT_MAX,right = INT_MAX,center = INT_MAX;
+        left = matrix[i][j] + solve(i+1,j-1,matrix,dp,n);
+        center = matrix[i][j] + solve(i+1,j,matrix,dp,n);
+        right = matrix[i][j] + solve(i+1,j+1,matrix,dp,n);
 
-    //     return dp[i][j] = min({down,dleft,dright});
-    // }
-
+        return dp[i][j] = min({left,center,right});
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
+        vector<vector<int>> dp(n, vector<int> (n,-1));
 
-        // long mini = INT_MAX;
-        // for(int j=0;j<matrix[0].size();j++){
-        //    mini = min(mini,solve(0,j,matrix,dp));
+        // int res = INT_MAX;
+        // for(int i=0;i<n;i++){
+        //     res = min(res,solve(0,i,matrix,dp,n));
         // }
-
-        // return (int)mini;
+        // return res;
 
         for(int i=0;i<n;i++){
             dp[0][i] = matrix[0][i];
         }
-
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                if(j==0){
+                if(j == 0){
                     dp[i][j] = matrix[i][j] + min(dp[i-1][j],dp[i-1][j+1]);
                 }
-                else if(j == n-1){
-                    dp[i][j] = matrix[i][j] + min({dp[i-1][j],dp[i-1][j-1]});
+                else if(j==n-1){
+                    dp[i][j] = matrix[i][j] + min({dp[i-1][j-1],dp[i-1][j]});
                 }
                 else{
                     dp[i][j] = matrix[i][j] + min({dp[i-1][j-1],dp[i-1][j],dp[i-1][j+1]});
                 }
             }
         }
-
         int res = INT_MAX;
-        for(int i=0;i<n;i++){
-            res = min(res,dp[n-1][i]);
+        for(int j=0;j<n;j++){
+            res = min(res,dp[n-1][j]);
         }
 
         return res;
